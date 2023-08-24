50260be67326081ae25919306e07bcac93c80dd4
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-08-24 16:35:25 +0200

    index and js: add milestone 2

--- js/main.js
+++ js/main.js
@@ -39,11 +39,28 @@ const team = [
 
 // stampare su console per ogni membro del team le informazioni
 
+// for (const teamWork of team) {
+//   // nome
+//   console.log(teamWork.name);
+//   // ruolo
+//   console.log(teamWork.job);
+//   // la stringa della foto
+//   console.log(teamWork.img);
+// }
+
+//# MILESTONE 2
+
+// stampare le stesse informazioni su DOM sottoforma di stringhe
+
+const cardsContainer = document.getElementById("cards-container");
+
 for (const teamWork of team) {
-  // nome
-  console.log(teamWork.name);
-  // ruolo
-  console.log(teamWork.job);
-  // la stringa della foto
-  console.log(teamWork.img);
+  // stampo nome
+  cardsContainer.innerHTML += "Nome: " + teamWork.name + "<br/>";
+
+  // stampo ruolo
+  cardsContainer.innerHTML += "Ruolo: " + teamWork.job + "<br/>";
+
+  // stampo la stringa della foto
+  cardsContainer.innerHTML += "Stringa foto: " + teamWork.img + "<br/>";
 }
