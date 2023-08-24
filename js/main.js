67c3b14fce2b7697747a966c93f9bd7b08734502
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-08-24 15:46:00 +0200

    js: add array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+//# MILESTONE 0
 
+// creare l’array di oggetti con le informazioni fornite.
+
+const team = [
+  {
+    name: "Wayne Barnett",
+    job: "Founder & CEO",
+    img: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    name: "Angela Caroll",
+    job: "Chief Editor",
+    img: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter Gordon",
+    job: "Office Manager",
+    img: "walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela Lopez",
+    job: "Social Media Manager",
+    img: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott Estrada",
+    job: "Developer",
+    img: "scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara Ramos",
+    job: "Graphic Designer",
+    img: "barbara-ramos-graphic-designer.jpg",
+  },
+];
